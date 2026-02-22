d87c65385921bb37664aa2136ae220f1df324c19
Amit Choraria <jainamit130@gmail.com>
2026-02-22 18:45:09 +0530

    Update environment.js

--- src/config/environment.js
+++ src/config/environment.js
@@ -6,8 +6,8 @@ const ENVIRONMENT = {
     USER_SUBBASE_URL: "/converse/users",
   },
   production: {
-    USER_BASE_URL: "https://user-service-841891702105.asia-south1.run.app",
-    CHAT_BASE_URL: "https://chat-service-841891702105.asia-south1.run.app",
+    USER_BASE_URL: "https://user-service-32563924728.asia-south1.run.app",
+    CHAT_BASE_URL: "https://chat-service-32563924728.asia-south1.run.app",
     CHAT_SUBBASE_URL: "/converse/chat",
     USER_SUBBASE_URL: "/converse/users",
   },
